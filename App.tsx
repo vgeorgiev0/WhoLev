19b8681c59979f52da5cf4c2f31f64ed045c39c4
VGeorgiev <vgeorgiev@up2technology.com>
2022-09-17 18:06:37 +0300

    feat: Display basic post

--- App.tsx
+++ App.tsx
@@ -1,10 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Vadim Savin",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.post}>
+        {/* Header */}
+        <View style={styles.header}>
+          <Image
+            source={{ uri: post.User.image }}
+            style={styles.profileImage}
+          />
+          <View>
+            <Text style={styles.name}>{post.User.name}</Text>
+            <Text style={styles.subtitle}>{post.createdAt}</Text>
+          </View>
+          <Entypo
+            name="dots-three-horizontal"
+            size={18}
+            color="gray"
+            style={styles.icon}
+          />
+        </View>
+        {/* Body */}
+        {post.description && (
+          <Text style={styles.description}>{post.description}</Text>
+        )}
+        {post.image && (
+          <Image source={{ uri: post.image }} style={styles.image} />
+        )}
+        {/* Footer */}
+        <View style={styles.footer}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +56,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  post: {
+    width: "100%",
+    marginVertical: 10,
+    backgroundColor: "#fff",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  profileImage: {
+    height: 40,
+    width: 40,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  subtitle: { color: "gray" },
+  icon: {
+    marginLeft: "auto",
+  },
+  description: {
+    paddingHorizontal: 10,
+    lineHeight: 20,
+    letterSpacing: 0.5,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    marginTop: 10,
+  },
+  footer: {},
 });
